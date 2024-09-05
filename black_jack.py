12dca4b99ee26ea8f7cd200167ac34ba101b2ed7
Edibulb <gnsqud24@naver.com>
2024-09-05 13:20:29 +0900

    Implemented calculated_score function

--- black_jack.py
+++ black_jack.py
@@ -24,6 +24,14 @@ def deal_card():
 
 def calculate_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0  # 0 represents a blackjack.
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    
+    return sum(cards)
 
     # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
     # Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
